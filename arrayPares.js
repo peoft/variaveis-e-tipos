ccb4cc3764ac6c19f89e047f3167150923e41439
peoft <paulo.ostermann.filho@gmail.com>
2022-07-03 11:24:10 -0300

    Variaveis e tipos

---
+++ arrayPares.js
@@ -0,0 +1,21 @@
+function substituiPares(array) {
+    if (!array) {
+        return -1;
+    }
+    if (!array.length) {
+        return -1;
+    }    
+
+    for (let i =0; i < array.length; i++) {
+        if (array[i] != 0 && array[i] % 2 === 0) {
+            array[i] = 0;
+        }
+    }
+    return array;
+}
+
+
+let array;
+//let array = [1, 3, 4, 6, 80, 33, 23, 90];
+
+console.log(substituiPares(undefined));
